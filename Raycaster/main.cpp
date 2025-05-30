3a2512c48218abffd1ec48021352c3b265d40f63
Michel Dekiere <michel.dekiere@ugent.be>
2025-05-30 22:10:01 +0200

    Added 2D map drawing

--- Raycaster/main.cpp
+++ Raycaster/main.cpp
@@ -2,9 +2,26 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "SFML works!");
-    sf::CircleShape shape(100.f);
+    unsigned int winHeight = 800;
+    unsigned int winWidth = winHeight;
+
+    sf::RenderWindow window(sf::VideoMode({ winHeight, winWidth }), "Raycaster");
+    sf::RectangleShape shape({ 100-10, 100-10 });
     shape.setFillColor(sf::Color::Green);
+    shape.setOutlineThickness(10.f);
+    shape.setOutlineColor(sf::Color(250, 150, 100));
+
+    int map[8][8] =
+    {
+        {1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,1},
+        {1,0,0,1,1,1,1,1},
+        {1,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1}
+    };
 
     while (window.isOpen())
     {
@@ -15,7 +32,19 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+
+                if (map[j][i] == 1) {
+
+                    sf::Transform t;
+                    t.translate({ (float)i*100, (float)j*100 });
+                    window.draw(shape, t);
+                }
+
+            }
+
+        }
         window.display();
     }
 }
